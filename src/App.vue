6ca301e4e18d0c6d38c8b5e34614ef7a0da5cc8b
kristina <kistina@localhost>
2023-01-10 17:28:47 +0300

    vue-resource

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container app">
+    <h1 class="mt-5 mb-3">Home page</h1>
+    <div class="form-group pt-2 pb-2">
+      <label for="name">Car name</label>
+      <input type="text" id="name" class="form-control" v-model.trim="carName" />
+    </div>
+    <div class="form-group pt-2 pb-2">
+      <label for="year">Car year</label>
+      <input type="text" id="year" class="form-control" v-model.number="carYear" />
+    </div>
+
+    <div class="row">
+      <div class="col-4 d-flex justify-content-between">
+        <button class="btn btn-success mt-3" @click.prevent="createCar">Create</button>
+        <button class="btn btn-primary mt-3 ml-3" @click.prevent="loadCar">Load</button>
+      </div>
+    </div>
+
+    <hr>
+
+    <ul class="list-group">
+      <li class="list-group-item" v-for="car in cars" :key="car.id">
+        Name: {{ car.name }} | Year: {{ car.year }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      carName: '',
+      carYear: 2014,
+      cars: [],
+      resourse: null
+    }
+  },
+  methods: {
+    createCar() {
+      this.resourse.save({}, { name: this.carName, year: this.carYear })
+    },
+    loadCar() {
+      this.resourse.get().then(res => res.json()).then(arr => {
+        this.cars = arr
+      })
+    }
+  },
+  created() {
+    this.resourse = this.$resource('cars')
   }
 }
 </script>
 
 <style>
-#app {
+.app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import VueResource from "vue-resource";
 
-Vue.config.productionTip = false
+Vue.use(VueResource);
+
+Vue.http.options.root = "http://localhost:3000/";
+
+Vue.http.interceptors.push((req) => {
+  req.headers.set("Auth", "TOKEN" + Math.random());
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
